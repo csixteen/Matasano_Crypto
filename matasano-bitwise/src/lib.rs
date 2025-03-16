25c64276da325db32c4a2bda3bc13680f073d324
Pedro Rodrigues <csixteen@protonmail.com>
2025-03-16 10:42:57 +0100

    Challenge 5 - repeating-key XOR

--- matasano-bitwise/src/lib.rs
+++ matasano-bitwise/src/lib.rs
@@ -33,6 +33,31 @@ macro_rules! boolean_ops {
                     i += 1;
                 }
             }
+
+            pub fn [<$name _cycle>](a: impl AsRef<[u8]>, b: impl AsRef<[u8]>) -> Vec<u8> {
+                let a = a.as_ref();
+                let mut res = Vec::new();
+                let mut i = 0;
+                let mut b = b.as_ref().iter().cycle();
+
+                while i < a.len() {
+                    res.push(u8::$op(a[i], b.next().unwrap()));
+                    i += 1;
+                }
+
+                res
+            }
+
+            pub fn [<$name _cycle_mut>](mut a: impl AsMut<[u8]>, b: impl AsRef<[u8]>) {
+                let a = a.as_mut();
+                let mut b = b.as_ref().iter().cycle();
+                let mut i = 0;
+
+                while i < a.len() {
+                    a[i] = u8::$op(a[i], b.next().unwrap());
+                    i += 1;
+                }
+            }
         }
     )*};
 }
@@ -54,6 +79,19 @@ mod tests {
                 "1c0111001f010100061a024b53535009181c",
                 "686974207468652062756c6c277320657965"
             )
+        );
+    }
+
+    #[test]
+    fn test_xor_cycle() {
+        let a = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        let b = "ICE";
+
+        assert_eq!(
+            hex_str_to_bytes(
+                "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+            ),
+            xor_cycle(a, b)
         );
     }
 }
